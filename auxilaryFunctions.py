f61e35a6d6274b07ec6fa40dd3a4ed7f48d65133
Aleksa Racic <racicaleksa7@gmail.com>
2022-06-19 21:22:34 +0200

    Dodani grafici po godini i roku

--- auxilaryFunctions.py
+++ auxilaryFunctions.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+from matplotlib.pyplot import figure
 
 def podaci_po_godinama(df):
     godine = []
@@ -13,16 +14,37 @@ def podaci_po_godinama(df):
     return godine, izaslo, prijavilo
 
 def podaci_po_mesecima(df):
-    meseci = ['јануар', 'фебруар', 'јун', 'јул','август', 'септембар', 'октобар']
-    godine = []
+    meseci_ = ['јануар', 'фебруар', 'јун', 'јул','август', 'септембар', 'октобар']
+    meseci = []
     izaslo = []
     prijavilo = []
-    for mesec in meseci:
+    for mesec in meseci_:
         godina_df = df[df['tip_roka'] == mesec]
-        godine.append(mesec)
+        meseci.append(mesec)
         izaslo.append(sum(godina_df['ukupno_prijavilo']))
         prijavilo.append(sum(godina_df['ukupno_izaslo']))
-    return godine, izaslo, prijavilo
+    return meseci, izaslo, prijavilo
+
+def po_godinama_mesecima(df):
+    meseci_ = ['јануар', 'фебруар', 'јун', 'јул','август', 'септембар', 'октобар']
+    po_godinama = {}
+    for godina in df['skolska_godina_roka'].unique():
+        godina_df = df[df['skolska_godina_roka'] == godina]
+        izaslo = []
+        prijavilo = []
+        for mesec in meseci_:
+            mesec_df = godina_df[df['tip_roka'] == mesec]
+            prijavilo.append(sum(mesec_df['ukupno_prijavilo']))
+            izaslo.append(sum(mesec_df['ukupno_izaslo']))
+        po_godinama[godina]={
+            'godina' : godina,
+            'izaslo' : izaslo,
+            'prijavilo' : prijavilo
+        }
+    return po_godinama
+        
+
+
 
 def custom_plot(labels, data, title="",  ylabel="", xlabel="", name=""):
     ticks = range(len(labels))
@@ -32,5 +54,21 @@ def custom_plot(labels, data, title="",  ylabel="", xlabel="", name=""):
     plt.xlabel(xlabel)
     plt.title(title)
     plt.ylabel(ylabel)
+    plt.grid()
+    plt.savefig('Grafici/'+name+'_grafik.png')
+
+def custom_multitrend_plot(data_list , title="",  ylabel="", xlabel="", name=""):
+    figure(figsize=(8, 6), dpi=300)
+    labels = ['јануар', 'фебруар', 'јун', 'јул','август', 'септембар', 'октобар']
+    ticks = range(len(labels))
+    plt.figure(tight_layout=True)
+    plt.xticks(ticks, labels)
+    for data in data_list:
+        print(data['opis'])
+        plt.plot(data['data'], 'o-', linewidth=2, color=data['color'], label = data['opis'])
+    plt.xlabel(xlabel)
+    plt.title(title)
+    plt.ylabel(ylabel)
+    plt.legend()
     plt.grid()
     plt.savefig('Grafici/'+name+'_grafik.png')
